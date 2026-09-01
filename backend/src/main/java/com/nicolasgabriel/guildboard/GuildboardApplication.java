5dcd95b02a83d1e04d8564a9b52826d05f5e11ef
nicolas-riera <nicolas.riera@laplateforme.io>
2026-09-01 14:04:32 +0200

    feat: added DB credentials (don't worry they are local)

--- backend/src/main/java/com/nicolasgabriel/guildboard/GuildboardApplication.java
+++ backend/src/main/java/com/nicolasgabriel/guildboard/GuildboardApplication.java
@@ -2,10 +2,8 @@ package com.nicolasgabriel.guildboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-// @SpringBootApplication
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) // Remove when database is set up
+@SpringBootApplication
 public class GuildboardApplication {
 
 	public static void main(String[] args) {
