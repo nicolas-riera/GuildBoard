55193c1e6fd886a955a9f7d466d8dc43665c52d7
nicolas-riera <nicolas.riera@laplateforme.io>
2026-09-02 12:42:30 +0200

    add(wip): added enums and added entity classes

---
+++ backend/src/main/java/com/forgesoft/guildboard/entity/Quest.java
@@ -0,0 +1,10 @@
+package com.forgesoft.guildboard.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="\"Quest\"")
+public class Quest {
+    
+}
