5a393bd4a3c44d4bd5488193cf0a296d29745938
nicolas-riera <nicolas.riera@laplateforme.io>
2026-09-02 10:56:56 +0200

    refactor: changed project name and dependencies due to requirements

--- backend/src/main/java/com/forgesoft/guildboard/GuildboardApplication.java
+++ backend/src/main/java/com/forgesoft/guildboard/GuildboardApplication.java
@@ -1,4 +1,4 @@
-package com.nicolasgabriel.guildboard;
+package com.forgesoft.guildboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/forgesoft/guildboard/GuildboardApplicationTests.java
+++ backend/src/test/java/com/forgesoft/guildboard/GuildboardApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nicolasgabriel.guildboard;
+package com.forgesoft.guildboard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
