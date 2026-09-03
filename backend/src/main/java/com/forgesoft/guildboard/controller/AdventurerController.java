476d36748a2cdc68cecdd7fe99276ebc4cf292db
nicolas-riera <nicolas.riera@laplateforme.io>
2026-09-03 11:26:43 +0200

    feat(wip): added get method for /api/adventurers

--- backend/src/main/java/com/forgesoft/guildboard/controller/AdventurerController.java
+++ backend/src/main/java/com/forgesoft/guildboard/controller/AdventurerController.java
@@ -1,5 +1,30 @@
 package com.forgesoft.guildboard.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.forgesoft.guildboard.entity.Adventurer;
+import com.forgesoft.guildboard.service.AdventurerService;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/adventurers/")
 public class AdventurerController {
+
+    private final AdventurerService service;
+
+    public AdventurerController(AdventurerService service) {
+        this.service = service;
+    }
+
+    @GetMapping("path")
+    public List<Adventurer> getAll() {
+        return service.getAll();
+    }
+    
     
 }
--- backend/src/main/java/com/forgesoft/guildboard/service/AdventurerService.java
+++ backend/src/main/java/com/forgesoft/guildboard/service/AdventurerService.java
@@ -1,5 +1,26 @@
 package com.forgesoft.guildboard.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.forgesoft.guildboard.entity.Adventurer;
+import com.forgesoft.guildboard.repository.AdventurerRepository;
+import com.forgesoft.guildboard.repository.AssignmentRepository;
+
+@Service
 public class AdventurerService {
+
+    private final AdventurerRepository adventurerRepository;
+    private final AssignmentRepository assignmentRepository;
+
+    public AdventurerService(AdventurerRepository adventurerRepository, AssignmentRepository assignmentRepository) {
+        this.adventurerRepository = adventurerRepository;
+        this.assignmentRepository = assignmentRepository;
+    }
+
+    public List<Adventurer> getAll() {
+        return adventurerRepository.findAll();
+    }
     
 }
